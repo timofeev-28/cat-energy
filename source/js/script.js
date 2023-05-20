558017ded10bfa7c4c84a2e1734c9581c26009dd
Dmitrii Timofeev <28dngn@gmail.ru>
2023-05-20 15:53:24 +0300

    бургер-меню добавлено

---
+++ source/js/script.js
@@ -0,0 +1,21 @@
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.main-nav');
+let menuLinks = menu.querySelectorAll('.main-nav__link');
+let header = document.querySelector('.header');
+
+// когда JS есть, .js-on прячет бургер-меню
+header.classList.add('header--js-on');
+menu.classList.add('main-nav--js-on');
+burger.classList.remove('visually-hidden');
+
+burger.addEventListener('click', function () {
+  menu.classList.toggle('main-nav--active');
+  burger.classList.toggle('burger--active');
+});
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function (event) {
+    menu.classList.remove('nav--active');
+    burger.classList.remove('burger--active');
+  })
+});
